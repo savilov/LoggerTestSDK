01b6cf0fa1e53c7ee612695b5a766d2e2edaf622
Sergii Avilov <serega.avilov@gmail.com>
2019-02-06 15:56:40 +0200

    first commit

---
+++ Example/Pods/LoggerTestSDK.framework/Headers/LoggerTestSDK.h
@@ -0,0 +1,19 @@
+//
+//  LoggerTestSDK.h
+//  LoggerTestSDK
+//
+//  Created by Sergii Avilov on 2/6/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoggerTestSDK.
+FOUNDATION_EXPORT double LoggerTestSDKVersionNumber;
+
+//! Project version string for LoggerTestSDK.
+FOUNDATION_EXPORT const unsigned char LoggerTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggerTestSDK/PublicHeader.h>
+
+
